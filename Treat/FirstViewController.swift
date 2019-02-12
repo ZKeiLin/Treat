35274660ecb3eb57d2287e9499c00223fc1c39cd
Liam Brozik <lbrozik@uw.edu>
2019-02-11 16:45:59 -0800

    Initial Commit

---
+++ Treat/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Treat
+//
+//  Created by Liam Brozik on 2/11/19.
+//  Copyright © 2019 iSchool. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Treat/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treat
+//
+//  Created by Liam Brozik on 2/11/19.
+//  Copyright © 2019 iSchool. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
