66e73f0d595928ba96407a28120576d58c44d1fc
Kelden Lin <keldenlin@gmail.com>
2019-03-05 20:23:15 -0800

    Treat Category Table added.

--- Treat/AddTreatViewController.swift
+++ Treat/AddTreatViewController.swift
@@ -37,12 +37,35 @@ class TreatOnlineDataSource : NSObject, UITableViewDataSource
     }
 }
 
+class TreatOnlineCategoryDataSource : NSObject, UITableViewDataSource
+{
+    var data : [String] = []
+    init(_ elements : [String]) {
+        data = elements
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TreatCategoryCell") as! TreatCategoryCell
+        cell.name.text = data[indexPath.row]
+        
+        return cell
+    }
+}
+
+
 
 class AddTreatViewController: UIViewController, UITableViewDelegate {
     @IBOutlet weak var popularTableView: UITableView!
+    @IBOutlet weak var categoryTableView: UITableView!
     
     var treats : [Treat] = []
+    var categories : [String] = []
     var dataSource : TreatOnlineDataSource? = nil
+    var categoryDataSource : TreatOnlineCategoryDataSource? = nil
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("User selected row at \(indexPath)")
@@ -53,11 +76,17 @@ class AddTreatViewController: UIViewController, UITableViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set default Treat
+        // Initialize Popular Treat Table
         dataSource = TreatOnlineDataSource([])
         popularTableView.dataSource = dataSource
         popularTableView.delegate = self
         popularTableView.reloadData()
+        
+        // Initialize Category Treat Table
+        categoryDataSource = TreatOnlineCategoryDataSource([])
+        categoryTableView.dataSource = categoryDataSource
+        categoryTableView.delegate = self
+        categoryTableView.reloadData()
         
         // Fetch online Treats
         fetchJsonData("https://api.myjson.com/bins/zvc0e")
@@ -93,10 +122,18 @@ class AddTreatViewController: UIViewController, UITableViewDelegate {
                 
                 DispatchQueue.main.async {
                     print(fetchedTreats)
+                    // All Treats
                     self.treats = fetchedTreats
                     self.dataSource = TreatOnlineDataSource(self.treats)
                     self.popularTableView.dataSource = self.dataSource
                     self.popularTableView.reloadData()
+                    
+                    // Categories
+                    self.categories = self.getCategories(self.treats)
+                    self.categoryDataSource = TreatOnlineCategoryDataSource(self.categories)
+                    self.categoryTableView.dataSource = self.categoryDataSource
+                    self.categoryTableView.reloadData()
+
                 }
             }.resume()
         }
@@ -111,9 +148,28 @@ class AddTreatViewController: UIViewController, UITableViewDelegate {
         }
         
 //        returnTreat = returnTreat.sorted(by: { $0.hits > $1.hits })
+        self.categories = getCategories(returnTreat)
         
         return returnTreat
     }
     
+    func getCategories (_ treats : [Treat]) -> [String] {
+        var returnCategories : [String] = []
+        for t in treats {
+            if !returnCategories.contains(t.category) { returnCategories.append(t.category) }
+        }
+        print (returnCategories)
+        
+        return returnCategories
+    }
+    
+    func getTreatsFromCategory (_ category : String) -> [Treat] {
+        var returnTreat : [Treat] = []
+        for t in treats {
+            if t.category == category { returnTreat.append(t) }
+        }
+        
+        return returnTreat
+    }
     
 }
